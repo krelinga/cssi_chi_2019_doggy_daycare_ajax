04e291e4326f0bca2f2fb5c8ca720facfee0dd3e
Andy Kreling <krelinga@gmail.com>
2019-04-25 14:04:36 -0500

    Handle more JSON info from the server with client-side rendering.

--- static/index.js
+++ static/index.js
@@ -1,10 +1,13 @@
+function renderDogInfo(data) {
+  var template = Handlebars.compile(document.getElementById('dogAreaTemplate').innerHTML);
+  document.getElementById('dogArea').innerHTML = template(data);
+}
+
 document.addEventListener("DOMContentLoaded", () => {
   document.getElementById("dogArea").innerHTML = "loading dogs...";
   fetch("/api/getdogs")
     .then((response) => {
       return response.json();
     })
-    .then((data) => {
-      document.getElementById('dogArea').innerHTML = data.dogs;
-    });
+    .then(renderDogInfo);
 });
